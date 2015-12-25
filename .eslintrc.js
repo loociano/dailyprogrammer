490e23dbcdc90b1c7fe5016179ccd35b3488765a
Luciano Rubio <luciano@loociano.com>
2015-12-25 21:17:14 +0000

    Linter

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "rules": {
+        "indent": [
+            2,
+            2
+        ],
+        "quotes": [
+            1,
+            "single"
+        ],
+        "linebreak-style": [
+            0,
+            "windows"
+        ],
+        "semi": [
+            2,
+            "always"
+        ]
+    },
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended"
+};
