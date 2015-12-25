ecc79de90003a26daf362126c2298b760f5b44cc
Luciano Rubio <luciano@loociano.com>
2015-12-25 22:01:38 +0000

    Searching dictionary words

--- 246-intermediate-letter-splits/js/index.js
+++ 246-intermediate-letter-splits/js/index.js
@@ -1,5 +1,36 @@
 'use strict';
 
+var fs = require('fs');
+
+/* @return hashmap */  
+function getWords(filename){  
+  
+  var hashmap = {};
+  var words = fs.readFileSync(filename, 'utf8').split('\r\n');
+  
+  for(var i = 0; i < words.length; i++){
+    var word = words[i];
+    if (hashmap[word] === undefined){
+      hashmap[word] = true;
+    }
+  }
+  
+  return hashmap;
+}
+
+function search(wordMap, searchArray){
+
+  var results = [];
+
+  for (var i = 0; i < searchArray.length; i++){
+    var word = searchArray[i];
+    if (wordMap[word.toLowerCase()] !== undefined){
+      results.push(word);
+    }
+  }
+  return results;
+}
+
 function Node(data){
   this.data = data;
   this.children = [];
@@ -15,10 +46,10 @@ function numberToChar(number){
   return String.fromCharCode(number + 64);
 }
 
-function numberToTree(input, start, root){
+function numberToTree(input, start, root, output){
 
   if (start === input.length){
-    console.log(root.data); // LEAF
+    output.push(root.data); // leaf
     return;
   }
 
@@ -27,7 +58,7 @@ function numberToTree(input, start, root){
   if (start === input.length - 1){
     if (current > 0 && current < 10){
       var node = root.append(root.data + numberToChar(current));
-      console.log(node.data); // LEAF
+      output.push(node.data);
     }
     return;
   }
@@ -36,17 +67,18 @@ function numberToTree(input, start, root){
 
   if (current > 0 && current < 10 && next !== 0){
     var node = root.append(root.data + numberToChar(current));
-    numberToTree(input, start + 1, node);
+    numberToTree(input, start + 1, node, output);
   }
 
-  var currentAndNext = parseInt(input[start] + input[start+1]);
+  var currentAndNext = parseInt(input[start] + input[start + 1]);
   if (currentAndNext > 0 && currentAndNext < 27){
     var node = root.append(root.data + numberToChar(currentAndNext));
-    numberToTree(input, start + 2, node);
+    numberToTree(input, start + 2, node, output);
   }
   
 }
 
+var wordMap = getWords('../../resources/enable1.txt');
 process.stdin.setEncoding('utf8');
 console.log('Type a number: ');
 
@@ -54,7 +86,21 @@ process.stdin.on('data', function (input) {
   var time = process.hrtime();
   
   console.log('');
-  numberToTree(input.trim(), 0, new Node(''));
+  var output = [];
+  numberToTree(input.trim(), 0, new Node(''), output);
+
+  console.log(output.length + ' combinations');
+
+  var dictWords = search(wordMap, output);
+
+  if (dictWords.length > 0){
+
+    console.log('Dictionary Words: ');
+  
+    dictWords.forEach(function(i){
+      console.log(i);
+    });
+  }
   
   var diff = process.hrtime(time);
   console.log('Total: %d milliseconds', (diff[0] * 1e9 + diff[1])/ 1e6);
