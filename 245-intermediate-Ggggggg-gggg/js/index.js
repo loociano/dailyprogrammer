5aefea955108e63fd2ef85839f092e66b85eece7
Luciano Rubio <luciano@loociano.com>
2015-12-26 15:22:19 +0000

    Challenge #245-i with huffman

--- 245-intermediate-Ggggggg-gggg/js/index.js
+++ 245-intermediate-Ggggggg-gggg/js/index.js
@@ -1,11 +1,55 @@
 'use strict';
 var fs = require('fs');
 
+/* Examples:
+   @param input: [{symbol: 'a', w: 0.8}, {symbol: 'b', w: 0.2}]
+   @param symbols: ['0', '1'] 
+   @return {'a': 0, 'b': 1}
+   */
+function huffman(input, symbols){
+
+  var results = {};
+
+  while(input.length > 1){
+    
+    var node1 = input.pop();
+    var node2 = input.pop();
+
+    var node = {
+      w: node1.w + node2.w
+    };
+    node[symbols[1]] = node1;
+    node[symbols[0]] = node2;
+
+    input.push(node);
+  }
+
+  var queue = [];
+  queue.push(input.pop());
+
+  while (queue.length > 0){
+    var n = queue.shift();
+    var carry = n.code || '';
+    
+    symbols.forEach(function(bit){
+      if (n[bit] != null){
+        n[bit].code = carry + bit;
+        queue.push(n[bit]);
+      } else {
+        results[n.symbol] = n.code;
+      }
+    });
+
+  }
+  //console.log(results);
+  return results;
+}
+
 function load(filename){
   return fs.readFileSync(filename, 'utf8');
 }
 
-function loadEncoded(data){
+function getMessageWithKey(data){
   
   var input = data.split('\r\n');
   var key = input.splice(0, 1)[0].split(' ');
@@ -15,14 +59,19 @@ function loadEncoded(data){
     map[key[i+1]] = key[i];
   }
 
+  // Get symbols from keys
+  var symbols = Object.keys(map).join('').split('').filter(function(value,i,array){
+    return i == array.indexOf(value); // Discard duplicates
+  });
+
   return {
+    symbols: symbols,
     key: map, 
     message: input.join('\r\n')
   };
 }
 
-
-function decode(key, message){
+function decode(key, message, symbols){
 
   var output = [];
   var cursor = 0;
@@ -36,7 +85,7 @@ function decode(key, message){
     }
 
     var next = message[++cursor];
-    if (/(g|G)/.test(next)){
+    if (symbols.indexOf(next) > -1){
       buffer += next;
     } else {
       output.push(next);
@@ -45,32 +94,37 @@ function decode(key, message){
   return output.join('');
 }
 
-function encode(message){
+function genKey(message, symbols){
 
-  function genKey(message){
+  var letters = {};
+  var count = 0;
 
-    var letters = [];
-    for(var i = 0; i < message.length; i++){
-      var letter = message[i];
-      if (letters.indexOf(letter) > -1 || !/[a-zA-Z]/.test(letter)) continue;
-      letters.push(letter);
-    }
-
-    var digits = Math.ceil(Math.log(letters.length) / Math.log(2));
-    var keymap = {};
-
-    for (var j = 0; j < letters.length; j++){
-      var code = j.toString(2).replace(/0/g, 'g').replace(/1/g, 'G');
-      while(code.length < digits){
-        code = 'g' + code;
-      }
-      keymap[letters[j]] = code;
-    }
-    return keymap;
+  // Get letters and occurrences
+  for(var i = 0; i < message.length; i++){
+    var letter = message[i];
+    if (!/[a-zA-Z]/.test(letter)) continue;
+    if (!letters[letter]) letters[letter] = 0;
+    letters[letter]++;
+    count++; 
   }
 
+  // hashmap to ordered array
+  var ordered = [];
+  for(var l in letters){
+    ordered.push({symbol: l, w: letters[l]});
+  }
+
+  ordered.sort(function(a,b){
+    return a.w - b.w;
+  });
+
+  return huffman(ordered, symbols);
+}
+
+function encode(message, symbols){
+
   var result = [];
-  var keymap = genKey(message);
+  var keymap = genKey(message, symbols);
 
   var firstline = '';
   for (var key in keymap){
@@ -89,20 +143,29 @@ function encode(message){
   return result.join('');
 }
 
-var input1 = loadEncoded(load('../input1.txt'));
-console.log(decode(input1.key, input1.message));
+/********************************************************/
+
+var input1 = getMessageWithKey(load('../input1.txt'));
+console.log(decode(input1.key, input1.message, input1.symbols));
 
 console.log();
 
-var input2 = loadEncoded(load('../input2.txt'));
-console.log(decode(input2.key, input2.message));
+var input2 = getMessageWithKey(load('../input2.txt'));
+console.log(decode(input2.key, input2.message, input2.symbols));
 
 console.log();
 
-var decodeChallenge = loadEncoded(load('../decodeChallenge.txt'));
-console.log(decode(decodeChallenge.key, decodeChallenge.message));
+var decodeChallenge = getMessageWithKey(load('../decodeChallenge.txt'));
+console.log(decode(decodeChallenge.key, decodeChallenge.message, decodeChallenge.symbols));
 
 console.log();
 
-var encodeChallenge = loadEncoded(encode(load('../encodeChallenge.txt')));
-console.log(decode(encodeChallenge.key, encodeChallenge.message));
+var challenge = load('../encodeChallenge.txt');
+
+var encoded = encode(challenge, ['g', 'G']);
+console.log(encoded);
+
+console.log();
+
+var encodeChallenge = getMessageWithKey(encoded);
+console.log(challenge === decode(encodeChallenge.key, encodeChallenge.message, encodeChallenge.symbols));
